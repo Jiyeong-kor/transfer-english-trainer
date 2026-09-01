f82d5ff790ce03f471b20ec59ab235d7b9b7dc53
Jiyeong-kor <186702776+Jiyeong-kor@users.noreply.github.com>
2026-09-01 21:46:27 +0900

    fix: 객관식 모드를 오프라인 캐시에 포함

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "transfer-english-trainer-v5";
+const CACHE = "transfer-english-trainer-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,6 +8,7 @@ const ASSETS = [
   "./app.js",
   "./app-enhancements.js",
   "./app-ux-parity.js",
+  "./exam-mode.js",
   "./app-update.js",
   "./content/vocabulary.js",
   "./content/grammar-v2.js",
